5413507afc5d15f913baf59d41ae908c59646705
Fgp910 <f.gutierrezpena910@gmail.com>
2020-11-30 13:33:29 +0100

    Actualizado para el (forzoso) cambio de número de pareja. De una respetable casa de apuestas a un youtuber de minecraft. Vaya decepción

--- P3/run.sh
+++ P3/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 practica3.py --trace traza.pcap --mac 00:11:88:CC:33:C4 --ip_flujo_tcp 98.252.67.82 --port_flujo_udp 64136
+python3 practica3.py --trace traza.pcap --mac 00:11:88:CC:33:FC --ip_flujo_tcp 53.59.245.211 --port_flujo_udp 1944
